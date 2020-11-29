ffb329b1b6d4b3d185e27928862c4683b8cb76dd
mykolas.jarackas <mykolas.jarackas@gmail.com>
2020-11-29 13:12:34 +0200

    Added possibility to choose how many top files/folders to show

--- SizeTree.Console/SizeTreeApp.cs
+++ SizeTree.Console/SizeTreeApp.cs
@@ -4,6 +4,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Console = Colorful.Console;
 using System.Drawing;
+using System;
 
 namespace SizeTree.ConsoleApp
 {
@@ -16,6 +17,7 @@ namespace SizeTree.ConsoleApp
         private string _targetPath;
         private bool _includeSubDirs;
         private bool _writeToOutputFile;
+        private int _countOfTopItems;
         private readonly IFileService _fileService;
         private readonly IOutputService _outputService;
         public SizeTreeApp(IFileService fileService, IOutputService outputService)
@@ -27,7 +29,6 @@ namespace SizeTree.ConsoleApp
         public async Task<int> Run(string[] args, CancellationToken ct)
         {
             Console.WriteAscii("FILE SIZE TREE", Color.Red);
-
             if (args.Count() == 0)
             {
                 Console.Write("Path: ");
@@ -38,12 +39,16 @@ namespace SizeTree.ConsoleApp
                 Console.WriteLine("");
                 Console.Write("Write to output file?: ");
                 _writeToOutputFile = bool.Parse(Console.ReadLine());
+                Console.WriteLine("");
+                Console.Write("How many top files/folders to show?: ");
+                _countOfTopItems = int.Parse(Console.ReadLine());
             }
             HandleArgs(args);
             var result = await _fileService.CalculateFileSizes(_targetPath, _includeSubDirs);
             if (_writeToOutputFile)
                 await _outputService.WriteOutputToFile(result);
-            result.ForEach(x =>
+            Console.WriteLine($"Top {_countOfTopItems} biggest files:", Color.Yellow);
+            result.Take(_countOfTopItems).ToList().ForEach(x =>
             {
                 Console.WriteLine($"Name: {x.FileName} ({x.PathToFile})", Color.Green);
                 Console.WriteLine($"Size: {x.FormatedSize}", Color.Green);
@@ -55,12 +60,33 @@ namespace SizeTree.ConsoleApp
         }
         private void HandleArgs(string[] args)
         {
-            if (args.Count() == 1)
-                _targetPath = args[0];
-            if (args.Count() >= 2)
+            try
             {
-                _targetPath = args[0];
-                _includeSubDirs = bool.Parse(args[1]);
+                if (args.Count() == 1)
+                    _targetPath = args[0];
+                if (args.Count() >= 2)
+                {
+                    _targetPath = args[0];
+                    _includeSubDirs = bool.Parse(args[1]);
+                }
+                if (args.Count() >= 3)
+                {
+                    _targetPath = args[0];
+                    _includeSubDirs = bool.Parse(args[1]);
+                    _writeToOutputFile = bool.Parse(args[2]);
+                }
+                if (args.Count() >= 4)
+                {
+                    _targetPath = args[0];
+                    _includeSubDirs = bool.Parse(args[1]);
+                    _writeToOutputFile = bool.Parse(args[2]);
+                    _countOfTopItems = int.Parse(args[3]);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Can't parse arguments", Color.Red);
+                throw;
             }
         }
     }
