d91243f71a404c5dd138ac01e1ce3463a1662b8b
mykolas.jarackas <mykolas.jarackas@gmail.com>
2020-12-30 13:30:16 +0200

    Added really simple installer project (using Visual Studio Installer Projects).

--- SizeTree.WindowsFormsApp/MainWindow.cs
+++ SizeTree.WindowsFormsApp/MainWindow.cs
@@ -1,4 +1,5 @@
-﻿using SizeTree.Core.Models;
+﻿using Microsoft.Extensions.Logging;
+using SizeTree.Core.Models;
 using SizeTree.Core.Services;
 using System;
 using System.Collections.Generic;
@@ -14,82 +15,100 @@ namespace SizeTree.WindowsFormsApp
         private readonly IOutputService _outputService;
         private List<FolderSizeInfo> Folders;
         private List<FileSizeInfo> Files;
-        public MainWindow(IFileService fileService, IOutputService outputService)
+        private readonly ILogger<MainWindow> _logger;
+        public MainWindow(IFileService fileService, IOutputService outputService, ILogger<MainWindow> logger)
         {
             _fileService = fileService;
             _outputService = outputService;
+            _logger = logger;
             InitializeComponent();
             SetUiAsNotLoading();
         }
 
         private async void button1_Click(object sender, EventArgs e)
         {
-            var folderCount = 0;
-            if (subDirCheckBox.Checked)
+            try
             {
-                await Task.Run(async () =>
-                {
-                    folderCount = await _fileService.GetCountOfSubDirectories(textBox1.Text);
-                });
-                SetDynamicProgressBar(folderCount);
-            }
-            else
-                SetStaticProgressBar();
-
-            SetUiAsLoading();
-
-            await Task.Run(async () =>
-            {
+                var folderCount = 0;
                 if (subDirCheckBox.Checked)
                 {
-                    Folders = new List<FolderSizeInfo>();
-                    await foreach (var item in _fileService.CalculateFolderSizesAsyncStream(textBox1.Text, subDirCheckBox.Checked))
+                    await Task.Run(async () =>
                     {
-                        Folders.Add(item);
-                        PushProgressBar();
-                    }
+                        folderCount = await _fileService.GetCountOfSubDirectories(textBox1.Text);
+                    });
+                    SetDynamicProgressBar(folderCount);
                 }
                 else
-                    Folders = await _fileService.CalculateFolderSizes(textBox1.Text, subDirCheckBox.Checked);
-                
-                Files = Folders.SelectMany(x => x.Files).ToList();
+                    SetStaticProgressBar();
 
-                if (this.writeToFileCheckBox.Checked)
+                SetUiAsLoading();
+
+                await Task.Run(async () =>
                 {
-                    await _outputService.WriteOutputToFile(Files);
-                    await _outputService.WriteOutputToFile(Folders);
-                }
-                
-            });
+                    if (subDirCheckBox.Checked)
+                    {
+                        Folders = new List<FolderSizeInfo>();
+                        await foreach (var item in _fileService.CalculateFolderSizesAsyncStream(textBox1.Text, subDirCheckBox.Checked))
+                        {
+                            Folders.Add(item);
+                            PushProgressBar();
+                        }
+                    }
+                    else
 
-            richTextBox1.Text = "";
-            Folders.OrderByDescending(x => x.FolderSizeInBytes)
-                    .Take(int.Parse(comboBox1.SelectedItem.ToString()))
-                    .ToList()
-                    .ForEach(x =>
+                    Folders = await _fileService.CalculateFolderSizes(textBox1.Text, subDirCheckBox.Checked);
+
+                    Files = Folders.SelectMany(x => x.Files).ToList();
+
+                    if (this.writeToFileCheckBox.Checked)
+                    {
+                        await _outputService.WriteOutputToFile(Files);
+                        await _outputService.WriteOutputToFile(Folders);
+                    }
+
+                });
+
+                richTextBox1.Text = "";
+                Folders.OrderByDescending(x => x.FolderSizeInBytes)
+                        .Take(int.Parse(comboBox1.SelectedItem.ToString()))
+                        .ToList()
+                        .ForEach(x =>
+                        {
+                            richTextBox1.AppendText($"Name: {x.FolderName} ({x.PathToFolder})");
+                            richTextBox1.AppendText(Environment.NewLine);
+                            richTextBox1.AppendText($"Size: {x.FormatedSize}");
+                            richTextBox1.AppendText(Environment.NewLine);
+                            richTextBox1.AppendText($"File count: {x.FileCount}");
+                            richTextBox1.AppendText(Environment.NewLine);
+                            richTextBox1.AppendText("------------------------------------");
+                            richTextBox1.AppendText(Environment.NewLine);
+                            richTextBox1.GoToLine(0);
+                        });
+                richTextBox2.Text = "";
+                Files.Take(int.Parse(comboBox1.SelectedItem.ToString())).ToList().ForEach(x =>
+                {
+                    richTextBox2.AppendText($"Name: {x.FileName} ({x.PathToFile})");
+                    richTextBox2.AppendText(Environment.NewLine);
+                    richTextBox2.AppendText($"Size: {x.FormatedSize}");
+                    richTextBox2.AppendText(Environment.NewLine);
+                    richTextBox2.AppendText("------------------------------------");
+                    richTextBox2.AppendText(Environment.NewLine);
+                    richTextBox1.GoToLine(0);
+                });
+                SetUiAsNotLoading();
+            }
+
+            catch(Exception ex)
             {
-                richTextBox1.AppendText($"Name: {x.FolderName} ({x.PathToFolder})");
-                richTextBox1.AppendText(Environment.NewLine);
-                richTextBox1.AppendText($"Size: {x.FormatedSize}");
-                richTextBox1.AppendText(Environment.NewLine);
-                richTextBox1.AppendText($"File count: {x.FileCount}");
-                richTextBox1.AppendText(Environment.NewLine);
-                richTextBox1.AppendText("------------------------------------");
-                richTextBox1.AppendText(Environment.NewLine);
-                richTextBox1.GoToLine(0);
-            });
-            richTextBox2.Text = "";
-            Files.Take(int.Parse(comboBox1.SelectedItem.ToString())).ToList().ForEach(x =>
+                SetUiAsNotLoading();
+                _logger.LogError(ex.Message, ex);
+            }
+            finally
             {
-                richTextBox2.AppendText($"Name: {x.FileName} ({x.PathToFile})");
-                richTextBox2.AppendText(Environment.NewLine);
-                richTextBox2.AppendText($"Size: {x.FormatedSize}");
-                richTextBox2.AppendText(Environment.NewLine);
-                richTextBox2.AppendText("------------------------------------");
-                richTextBox2.AppendText(Environment.NewLine);
-                richTextBox1.GoToLine(0);
-            });
-            SetUiAsNotLoading();
+                Folders = null;
+                Files = null;
+                GC.Collect();
+            }
         }
         private void button2_Click(object sender, EventArgs e)
         {
